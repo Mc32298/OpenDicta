7b1e551a44c48c0430f6e54243cbd1499391c8dd
Mc32298 <mc32298@gmail.com>
2026-05-28 14:13:02 +0000

    Liu

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
+    host: "0.0.0.0",
+    allowedHosts: ["code.spinop.com"],
     // Tell Vite to watch everything except the Rust source
     watch: {
       ignored: ["**/src-tauri/**"],
